5ccdaadb4e306711d1f13b252f60954da0615f85
SuperFola <lexplt.dev@gmail.com>
2020-09-05 18:59:35 +0200

    updating code to use request module

--- nuclear/__main__.py
+++ nuclear/__main__.py
@@ -2,16 +2,63 @@
 
 import os
 import sys
+import argparse
 from typing import List
 
+from . import get
 
-def main(*args: List[str]) -> int:
+
+def is_valid_package_name(name: str) -> bool:
+    """
+    Check if a given package name is under the form user/repo
+    """
+    # we need a single /
+    if name.count('/') != 1:
+        return False
+    # we need a valid username and repo name, not user/ or /repo
+    if not all(len(e) > 0 for e in name.split('/')):
+        return False
+    return True
+
+
+def main() -> int:
     """
     The main function of the program
     """
-    print(*args)
+    parser = argparse.ArgumentParser(prog='nuclear', add_help=True)
+    subparsers = parser.add_subparsers(dest='subparsers')
+    install = subparsers.add_parser(
+        'install', help='Install an ArkScript package from GitHub'
+    )
+    install.add_argument(
+        'package',
+        help='Should look like this: user/repo'
+    )
+
+    remove = subparsers.add_parser(
+        'remove', help='Remove an ArkScript package'
+    )
+
+    args = parser.parse_args()
+    print(args)
+    if args.subparsers == 'install':
+        # check for validity
+        if not is_valid_package_name(args.package):
+            print(f"Provided package name is not valid: {args.package}")
+            return -1
+        # then check for existance on GitHub
+        user, repo = args.package.split('/')
+        if not get.check_user(user):
+            print(f"The GitHub user '{user}' doesn't exists")
+            return -1
+        if not get.check_repo(user, repo):
+            print(f"The wanted package '{repo}' from {user} couldn't be found on GitHub")
+            return -1
+    elif args.subparsers == 'remove':
+        pass
+
     return 0
 
 
 if __name__ == '__main__':
-    sys.exit(main(*sys.argv[1:]))
+    sys.exit(main())

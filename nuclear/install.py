bf15a896a4b853b440aa57b63d28b13b0981a855
Wei Yang <wyang_92@Hotmail.com>
2020-10-14 00:01:55 +0200

    update handler to handle different install mode

--- nuclear/install.py
+++ nuclear/install.py
@@ -14,15 +14,34 @@ from .utils import (
     get_dir,
 )
 
+def install_all() -> int:
+    package_dict = lockfile.parse_lockfile()
+    if not package_dict:
+        print('no packages to be installed')
+        return 0
+
+    for package_name,package_info in package_dict.items():
+        version = package_info[1]
+        print(f"installing {package_name} with version {version}")
+        install_single_package(package_info[0], package_info[1])
+
+    return 0
 
 def handle(args: argparse.Namespace) -> int:
+    # check if any argument passed
+    if args.package:
+        return install_single_package(args.package,args.version)
+    else:
+        return install_all()
+
+def install_single_package(package_name:str, version:str) -> int:
     # check for validity
-    if not package.is_valid_package_name(args.package):
-        log.error(f"Provided package name is not valid: {args.package}")
+    if not package.is_valid_package_name(package_name):
+        log.error(f"Provided package name is not valid: {package_name}")
         return -1
 
     # then check for existance on GitHub
-    user, repo = args.package.split('/')
+    user, repo = package_name.split('/')
 
     # first the user
     if not get.check_user(user):
@@ -35,7 +54,7 @@ def handle(args: argparse.Namespace) -> int:
         log.error(f"The wanted package '{repo}' from {user} couldn't be found on GitHub")
         return -1
 
-    tar_addr, used_version = get.search_tar(user, repo, args.version)
+    tar_addr, used_version = get.search_tar(user, repo, version)
     # error handling for when tar_addr is None has already been done
     if tar_addr is not None:
         # download
@@ -44,8 +63,8 @@ def handle(args: argparse.Namespace) -> int:
         filename = get_dir.get_filename(r.headers.get('content-disposition'))
         #try making the tarball
         try:  
-            makedirs(get_dir.get_dir_name(tar_addr,version=args.version), exist_ok=True)
-            with open(get_dir.get_dir_name(tar_addr,version=args.version)+"/"+filename,'wb',) as f:
+            makedirs(get_dir.get_dir_name(tar_addr,version=version), exist_ok=True)
+            with open(get_dir.get_dir_name(tar_addr,version=version)+"/"+filename,'wb',) as f:
                 f.write(r.content)
             sha256 = hashlib.sha256(r.content).hexdigest()
             lockfile.save(user, repo, used_version, tar_addr, sha256)

cddee20c78d7dd33992a59d396dc1fdad07c002e
Wei Yang <wyang_92@Hotmail.com>
2020-10-14 10:49:07 +0200

    code formating and proper logging

--- nuclear/install.py
+++ nuclear/install.py
@@ -14,27 +14,30 @@ from .utils import (
     get_dir,
 )
 
+
 def install_all() -> int:
     package_dict = lockfile.parse_lockfile()
     if not package_dict:
-        print('no packages to be installed')
-        return 0
+        log.error('no packages to be installed')
+        return -1
 
-    for package_name,package_info in package_dict.items():
+    for package_name, package_info in package_dict.items():
         version = package_info[1]
-        print(f"installing {package_name} with version {version}")
+        log.info(f"installing {package_name} with version {version}")
         install_single_package(package_info[0], package_info[1])
 
     return 0
 
+
 def handle(args: argparse.Namespace) -> int:
     # check if any argument passed
     if args.package:
-        return install_single_package(args.package,args.version)
+        return install_single_package(args.package, args.version)
     else:
         return install_all()
 
-def install_single_package(package_name:str, version:str) -> int:
+
+def install_single_package(package_name: str, version: str) -> int:
     # check for validity
     if not package.is_valid_package_name(package_name):
         log.error(f"Provided package name is not valid: {package_name}")
@@ -51,7 +54,8 @@ def install_single_package(package_name:str, version:str) -> int:
     # we could've just check the repo but checking for the user first
     # can help identify why we can't install a package
     if not get.check_repo(user, repo):
-        log.error(f"The wanted package '{repo}' from {user} couldn't be found on GitHub")
+        log.error(
+            f"The wanted package '{repo}' from {user} couldn't be found on GitHub")
         return -1
 
     tar_addr, used_version = get.search_tar(user, repo, version)
@@ -61,10 +65,11 @@ def install_single_package(package_name:str, version:str) -> int:
         r = requests.get(tar_addr)
         # get filename from content-dispositon
         filename = get_dir.get_filename(r.headers.get('content-disposition'))
-        #try making the tarball
-        try:  
-            makedirs(get_dir.get_dir_name(tar_addr,version=version), exist_ok=True)
-            with open(get_dir.get_dir_name(tar_addr,version=version)+"/"+filename,'wb',) as f:
+        # try making the tarball
+        try:
+            makedirs(get_dir.get_dir_name(
+                tar_addr, version=version), exist_ok=True)
+            with open(get_dir.get_dir_name(tar_addr, version=version)+"/"+filename, 'wb') as f:
                 f.write(r.content)
             sha256 = hashlib.sha256(r.content).hexdigest()
             lockfile.save(user, repo, used_version, tar_addr, sha256)
